2bf55e777b527d8a198f62d617727227c57a8bab
kalichkin22 <o.kalichkin@gmail.com>
2017-01-24 13:04:21 +0600

    Первая пробная попытка Range

--- Range/src/ru/academits/kalichkin/range/Range.java
+++ Range/src/ru/academits/kalichkin/range/Range.java
@@ -1,7 +1,7 @@
 package ru.academits.kalichkin.range;
 
 
-class Range {
+public class Range {
     private double from;
     private double to;
 
@@ -10,27 +10,23 @@ class Range {
         this.to = to;
     }
 
-    public double getFrom () {
+    public double getFrom() {
         return from;
     }
 
-    public void setFrom (double from) {
+    public void setFrom(double from) {
         this.from = from;
     }
 
-    public double getTo () {
+    public double getTo() {
         return to;
     }
 
-    public void setTo (double to) {
+    public void setTo(double to) {
         this.to = to;
     }
 
     public boolean isInside(double number) {
-
-        if (number > getTo() || number < getFrom()) {
-            return false;
-        }
-        return true;
+        return number >= getFrom() && number <= getTo();
     }
 }
