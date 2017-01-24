c3da11565be7af119e2072ec78fb0a102c41a265
kalichkin22 <o.kalichkin@gmail.com>
2017-01-24 13:04:34 +0600

    Первая пробная попытка Range

---
+++ Range/src/ru/academits/kalichkin/main/Main.java
@@ -0,0 +1,9 @@
+package ru.academits.kalichkin.main;
+import ru.academits.kalichkin.range.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Range interval = new Range(-10, 8);
+        System.out.println(interval.isInside(5.004));
+    }
+}
