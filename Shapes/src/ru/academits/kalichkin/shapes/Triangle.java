821c15e26fd4f06a08741d7796876d7060a23cf8
kalichkin22 <o.kalichkin@gmail.com>
2017-02-09 13:14:45 +0600

    Shapes

--- Shapes/src/ru/academits/kalichkin/shapes/Triangle.java
+++ Shapes/src/ru/academits/kalichkin/shapes/Triangle.java
@@ -19,8 +19,8 @@ public class Triangle implements Shape {
     }
 
 
-    private double getLength(double begin, double end, double begin2, double end2) {
-        return (Math.sqrt(Math.pow((end - begin), 2) + Math.pow((begin2 - end2), 2)));
+    private static double getLength(double x1, double x2, double y1, double y2) {
+        return (Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
 
     }
 
