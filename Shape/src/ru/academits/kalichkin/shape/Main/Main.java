7bf3fead08334c0c511a15856044fc87f88e58e0
kalichkin22 <o.kalichkin@gmail.com>
2017-02-10 15:36:25 +0600

    Shapes

--- Shape/src/ru/academits/kalichkin/shape/Main/Main.java
+++ Shape/src/ru/academits/kalichkin/shape/Main/Main.java
@@ -3,6 +3,17 @@ import ru.academits.kalichkin.shape.Shape.*;
 
 
 public class Main {
+    private static double getMaxArea(double[] a) {
+        double areaRecord = a[0];
+
+        for (int i = 1; i < a.length; ++i) {
+            if (a[i] > areaRecord) {
+                areaRecord = a[i];
+            }
+        }
+        return areaRecord;
+    }
+
     public static void main(String[] args) {
         Shape s1 = new Rectangle(10, 2);
         Shape s2 = new Triangle(-1, 2, 3, 1, 5, 4);
@@ -12,14 +23,14 @@ public class Main {
         Shape s6 = new Triangle(0, 1, 2, 3, 2, 3);
         Shape s7 = new Square(1);
         Shape s8 = new Circle(2);
-        Shape s9 = new Rectangle(6, 7);
-        Shape s10 = new Circle(1.5);
 
-        System.out.printf("%.2f, %.2f",s9.getArea(),s10.getArea());
+        double [] a = {s1.getArea(), s2.getArea(), s3.getArea(), s4.getArea(),
+                s5.getArea(), s6.getArea(), s7.getArea(), s8.getArea()};
 
+        System.out.println(getMaxArea(a));
     }
-
-
-
 }
+
+
+
 
