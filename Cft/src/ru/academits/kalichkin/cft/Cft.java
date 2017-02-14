504ca05df450211809c6f2def859b6eaeeadaed8
kalichkin22 <o.kalichkin@gmail.com>
2017-02-14 15:28:32 +0600

    Задача ЦФТ с целыми числами

--- Cft/src/ru/academits/kalichkin/cft/Cft.java
+++ Cft/src/ru/academits/kalichkin/cft/Cft.java
@@ -1,15 +1,14 @@
 package ru.academits.kalichkin.cft;
 
+import java.io.*;
 import java.util.Scanner;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 
 
 public class Cft {
     public static void main(String[] args) throws FileNotFoundException {
         try (Scanner scanner = new Scanner(new FileInputStream("input.txt"));
              PrintWriter writer = new PrintWriter("output.txt")) {
+
             while (scanner.hasNextInt()) {
                 int[] array = new int[6];
                 for (int i = 0; i < array.length; i++) {
@@ -17,6 +16,16 @@ public class Cft {
                 }
                 insertionSort(array);
                 for (int e : array) {
+                    writer.println(e);
+                }
+            }
+
+            while (scanner.hasNextLine()) {
+                for (int i = 0; i < args.length; i++) {
+                    args[i] = scanner.nextLine();
+                }
+                insertionSort(args);
+                for (String e : args) {
                     writer.println(e);
                 }
             }
@@ -35,6 +44,18 @@ public class Cft {
                 b--;
             }
             array[b + 1] = a;
+        }
+    }
+
+    public static void insertionSort(String[] strings) {
+        for (int j = 0; j < strings.length; j++) {
+            for (int i = j + 1; i < strings.length; i++) {
+                if (strings[i].compareTo(strings[j]) < 0) {
+                    String temp = strings[j];
+                    strings[j] = strings[i];
+                    strings[i] = temp;
+                }
+            }
         }
     }
 }
