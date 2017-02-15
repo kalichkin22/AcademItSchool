98a7e2c79b5d8d8a060a6d3b5e292458de155e1a
kalichkin22 <o.kalichkin@gmail.com>
2017-02-15 19:08:01 +0600

    Задача ЦФТ с целыми числами

--- Cft/src/ru/academits/kalichkin/cft/Cft.java
+++ Cft/src/ru/academits/kalichkin/cft/Cft.java
@@ -1,80 +1,46 @@
 package ru.academits.kalichkin.cft;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
 public class Cft {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        List<String> lines = new ArrayList<>();
 
-        if (args.length > 0) {
-            if (args[0].equals("in.txt out.txt -i -a")) {
-                try (Scanner scanner = new Scanner(new FileInputStream("in.txt"));
-                     PrintWriter writer = new PrintWriter("out.txt")) {
-
-                    int count = scanner.nextInt();
-                    int[] array = new int[count];
-
-                    while (scanner.hasNextInt()) {
-                        for (int i = 0; i < array.length; ++i) {
-                            array[i] = scanner.nextInt();
-                        }
-                    }
-
-                    insertionSort(array);
-                    for (int e : array) {
-                        writer.println(e);
-                    }
-
-                } catch (FileNotFoundException e) {
-                    System.out.println(e);
-                }
-            } else if (args[1].equals("in.txt out.txt -i -d")) {
-                try (Scanner scanner = new Scanner(new FileInputStream("in.txt"));
-                     PrintWriter writer = new PrintWriter("out.txt")) {
-
-                    int count = scanner.nextInt();
-                    int[] array = new int[count];
-
-                    while (scanner.hasNextInt()) {
-                        for (int i = 0; i < array.length; ++i) {
-                            array[i] = scanner.nextInt();
-                        }
-                    }
-
-                    insertSort(array);
-                    for (int e : array) {
-                        writer.println(e);
-                    }
-
-                } catch (FileNotFoundException e) {
-                    System.out.println(e);
-                }
-            } else if (args[2].equals("in.txt out.txt -s -a")) {
-                try (Scanner scanner = new Scanner(new FileInputStream("in.txt"));
-                     PrintWriter writer = new PrintWriter("out.txt")) {
-
-                    int count = scanner.nextInt();
-                    int[] array = new int[count];
-
-                    while (scanner.hasNextInt()) {
-                        for (int i = 0; i < array.length; ++i) {
-                            array[i] = scanner.nextInt();
-                        }
-                    }
-
-                    insertSort(array);
-                    for (int e : array) {
-                        writer.println(e);
-                    }
-
-                } catch (FileNotFoundException e) {
-                    System.out.println(e);
-                }
+        try (Scanner scanner = new Scanner(new FileInputStream(args[0]));
+             PrintWriter writer = new PrintWriter(args[1])) {
+            while (scanner.hasNextInt()) {
+                lines.add(scanner.nextLine());
             }
-        } else {
-            System.out.println("Такой команды нет");
+            String[] strings = lines.toArray(new String[lines.size()]);
+
+            int numbers[] = new int[strings.length];
+            for (int i = 0; i < strings.length; i++) {
+                numbers[i] = Integer.parseInt(strings[i]);
+            }
+
+            if (args.length > 0) {
+                if (args[0].equals("in.txt") && args[1].equals("out.txt") && args[2].equals("-i") && args[3].equals("-a")) {
+                    insertionSort(numbers);
+                    for (int e : numbers) {
+                        writer.println(e);
+                    }
+                } else if (args[0].equals("in.txt") && args[1].equals("out.txt") && args[2].equals("-i") && args[3].equals("-d")) {
+                    insertSort(numbers);
+                    for (int e : numbers) {
+                        writer.println(e);
+                    }
+                } else if (args[0].equals("in.txt") && args[1].equals("out.txt") && args[2].equals("-s") && args[3].equals("-a")) {
+//сюда пока не знаю как
+                }
+
+            } else {
+                System.out.println("Такой команды нет");
+            }
         }
     }
 
