45accaeb4173f7df88d46b55496b4f8d347b8a3d
kalichkin22 <o.kalichkin@gmail.com>
2017-02-17 23:13:03 +0600

    Vector part 1

---
+++ Vector/src/Main.java
@@ -0,0 +1,19 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        double[] array = {1, 2, 3, 4};
+
+        Vector vector = new Vector(1, 2, 3, 4, 5);
+        System.out.println(vector);
+
+        Vector vector1 = new Vector(vector);
+        System.out.println(vector1);
+
+        Vector vector2 = new Vector(10);
+        System.out.println(vector2);
+
+        Vector vector3 = new Vector(5, array);
+        System.out.println(vector3);
+    }
+}
