d015ea7375b9bc14d143a817b24560558bc59903
kalichkin22 <o.kalichkin@gmail.com>
2017-02-24 12:08:04 +0600

    ЦФТ ООП

--- Cft/src/ru/academits/kalichkin/cft/main/Main.java
+++ Cft/src/ru/academits/kalichkin/cft/main/Main.java
@@ -1,10 +1,44 @@
-
 package ru.academits.kalichkin.cft.main;
-import ru.academits.kalichkin.cft.commandsLine.CommandsLine;
+
+import ru.academits.kalichkin.cft.fileReader.FileReader;
+import ru.academits.kalichkin.cft.sortList.SortList;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        CommandsLine cft = new CommandsLine(args);
+        if (args.length != 4) {
+            System.out.println("Необходимо ввести четыре команды");
+            return;
+        }
+        boolean direction;
+        switch (args[3]) {
+            case "-a":
+                direction = false;
+                break;
+            case "-d":
+                direction = true;
+                break;
+            default:
+                throw new IllegalArgumentException("Такой команды нет");
+        }
+
+        switch (args[2]) {
+            case "-i": {
+                ArrayList<Integer> list = FileReader.readInt(args[0]);
+                SortList.insertionSort(list, Integer::compareTo, direction);
+                FileReader.writeFile(list, args[1]);
+                break;
+            }
+            case "-s": {
+                ArrayList<String> list = FileReader.readString(args[0]);
+                SortList.insertionSort(list, String::compareTo, direction);
+                FileReader.writeFile(list, args[1]);
+                break;
+            }
+
+            default:
+                throw new IllegalArgumentException("Такой команды нет");
+        }
     }
 }
 
--- Cft/src/ru/academits/kalichkin/cft/sortList/SortList.java
+++ Cft/src/ru/academits/kalichkin/cft/sortList/SortList.java
@@ -2,22 +2,19 @@ package ru.academits.kalichkin.cft.sortList;
 
 import java.util.Comparator;
 import java.util.List;
-import ru.academits.kalichkin.cft.programCommands.ProgramCommands;
 
 public class SortList {
     public static <T> void insertionSort(List<T> list, Comparator<T> tComparator, boolean value) {
         for (int i = 1; i < list.size(); i++) {
 
-            T a = list.get(i);
-            int b = i - 1;
+            T obj = list.get(i);
+            int j = i - 1;
 
-            while (b >= 0 && (tComparator.compare(list.get(b), a) < 0) == value) {
-                list.set(list.indexOf(list.get(b + 1)), list.get(b));
-                list.set(list.indexOf(list.get(b)), list.get(b + 1));
-                b--;
+            while (j >= 0 && (tComparator.compare(list.get(j), obj) < 0) == value) {
+                list.set(j + 1, list.get(j));
+                j--;
             }
-            list.set(list.indexOf(list.get(b + 1)), a);
-            list.set(list.indexOf(a), list.get(b + 1));
+            list.set(j + 1, obj);
         }
     }
 }
