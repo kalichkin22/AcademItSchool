b6b98621648f61863503d0543737ace6b1476fde
kalichkin22 <o.kalichkin@gmail.com>
2017-03-11 13:52:36 +0600

    Попытка сделать CSV в ООП

--- Csv/src/ru/academits/kalichkin/csv/Main.java
+++ Csv/src/ru/academits/kalichkin/csv/Main.java
@@ -1,22 +1,45 @@
+
 package ru.academits.kalichkin.csv;
+
+import ru.academits.kalichkin.csv.line.Line;
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("csv.txt")));
-             PrintWriter writer = new PrintWriter("html.txt")) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+             PrintWriter writer = new PrintWriter(args[1])) {
 
-            StringBuilder sb = new StringBuilder();
-            String cell = "";
-            sb.append("<table>").append(System.lineSeparator());
-
-            int c;
-            while ((c = reader.read()) != -1) {
-                sb.append((char) c);
+            String line = null;
+            Scanner scanner = null;
+            int index = 0;
+            List<Line> lineList = new ArrayList<>();
+            while ((line = reader.readLine()) != null) {
+                Line cell = new Line();
+                scanner = new Scanner(line);
+                scanner.useDelimiter(",");
+                while (scanner.hasNext()) {
+                    String data = scanner.next();
+                    if (index == 0) {
+                        cell.setFirstCell(data);
+                    } else if (index == 1) {
+                        cell.setSecondCell(data);
+                    } else if (index == 2) {
+                        cell.setThirdCell(data);
+                    } else {
+                        System.out.println("Некорректные данные::" + data);
+                    }
+                    index++;
+                }
+                index = 0;
+                lineList.add(cell);
             }
-
-            System.out.println(sb);
-            writer.print(sb);
+            writer.print(lineList.toString().replace("[", "<table>").replace("]", "").replace(",", "")
+                    + System.lineSeparator() + "</table>");
         }
     }
 }
+
