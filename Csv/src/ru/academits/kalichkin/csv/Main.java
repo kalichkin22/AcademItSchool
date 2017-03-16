f0d109258173ccbaa96281eea612eefd02443033
kalichkin22 <o.kalichkin@gmail.com>
2017-03-16 12:10:11 +0600

    Разбор CSV

--- Csv/src/ru/academits/kalichkin/csv/Main.java
+++ Csv/src/ru/academits/kalichkin/csv/Main.java
@@ -7,28 +7,46 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        List<String> list = new ArrayList<>();
-
         try (Scanner scanner = new Scanner(new FileInputStream(args[0]));
              PrintWriter writer = new PrintWriter(args[1])) {
+            List<String> list = new ArrayList<>();
 
             while (scanner.hasNextLine()) {
                 list.add(scanner.nextLine());
             }
 
             StringBuilder table = new StringBuilder();
-            StringBuilder line = new StringBuilder();
-            String cell = "";
+
+            table.append("<html>").append(System.lineSeparator());
+            table.append(" <head>").append(System.lineSeparator());
+            table.append("  <meta charset = \"utf-8\">").append(System.lineSeparator());
+            table.append("  <title> Разбор формата CSV </title>").append(System.lineSeparator());
+            table.append(" </head>").append(System.lineSeparator());
+            table.append(" <body>").append(System.lineSeparator());
+            table.append("  <table bordercolor=\"black\" border=\"1\" width=\"80%\">").append(System.lineSeparator());
 
             for (String e : list) {
-
-                for (int i = 0; i < e.length(); i++) {
-                    char c = e.charAt(i);
-                    cell += c;
+                table.append("<tr><td>");
+                int i = 0;
+                for (int j = 0; j < e.length(); j++) {
+                    char c = e.charAt(j);
+                    if (c == '"') {
+                        e = e.replace(",", " </td><td>");
+                        if (j > i) {
+                            table.append(e.substring(i, j));
+                        }
+                        i = j + 1;
+                    }
                 }
-                table.append(e).append("</td></tr><br/>").append(System.lineSeparator());
+                if (i < e.length()) {
+                    table.append(e.substring(i));
+                }
+                table.append("</td></tr><br/>").append(System.lineSeparator());
             }
 
+            table.append("  </table>").append(System.lineSeparator());
+            table.append(" </body>").append(System.lineSeparator());
+            table.append("</html>").append(System.lineSeparator());
             writer.print(table);
         }
     }
