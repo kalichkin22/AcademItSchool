de1cc4500941afdb16e37924e2178e0d9d93994e
kalichkin22 <o.kalichkin@gmail.com>
2017-03-19 12:18:29 +0600

    Разбор CSV

---
+++ Csv/src/ru/academits/kalichkin/csv/FileReader.java
@@ -0,0 +1,39 @@
+package ru.academits.kalichkin.csv;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class FileReader {
+    static List<String> readFile(String fileName) {
+        ArrayList<String> list = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new FileInputStream(fileName))) {
+
+            while (scanner.hasNextLine()) {
+                list.add(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.printf("Файл %s не найден%n", fileName);
+        }
+        return list;
+    }
+
+
+    static void writeFile(StringBuilder sb, String fileNameOut) {
+        try (PrintWriter writer = new PrintWriter(fileNameOut)) {
+            for (int i = 0; i < sb.length(); ++i) {
+                char c = sb.charAt(i);
+                writer.print(c);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.printf("Файл %s не найден%n", fileNameOut);
+        }
+    }
+}
+
+
+
+
--- Csv/src/ru/academits/kalichkin/csv/Main.java
+++ Csv/src/ru/academits/kalichkin/csv/Main.java
@@ -1,78 +1,68 @@
 package ru.academits.kalichkin.csv;
 
-import java.io.*;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        try (Scanner scanner = new Scanner(new FileInputStream(args[0]));
-             PrintWriter writer = new PrintWriter(args[1])) {
+    public static void main(String[] args) {
+        List<String> list = FileReader.readFile(args[0]);
+        StringBuilder table = new StringBuilder();
 
-            List<String> list = new ArrayList<>();
-            StringBuilder table = new StringBuilder();
+        table.append("<html>").append(System.lineSeparator());
+        table.append(" <head>").append(System.lineSeparator());
+        table.append("  <meta charset = \"utf-8\">").append(System.lineSeparator());
+        table.append("  <title> Разбор формата CSV </title>").append(System.lineSeparator());
+        table.append(" </head>").append(System.lineSeparator());
+        table.append(" <body>").append(System.lineSeparator());
+        table.append("  <table bordercolor=\"black\" border=\"1\" width=\"80%\">").append(System.lineSeparator());
 
-            while (scanner.hasNextLine()) {
-                list.add(scanner.nextLine());
+        boolean isQuoted = false;
+        int quote = 0;
+        for (String e : list) {
+            if (!isQuoted) {
+                table.append("<tr><td>");
             }
 
-            table.append("<html>").append(System.lineSeparator());
-            table.append(" <head>").append(System.lineSeparator());
-            table.append("  <meta charset = \"utf-8\">").append(System.lineSeparator());
-            table.append("  <title> Разбор формата CSV </title>").append(System.lineSeparator());
-            table.append(" </head>").append(System.lineSeparator());
-            table.append(" <body>").append(System.lineSeparator());
-            table.append("  <table bordercolor=\"black\" border=\"1\" width=\"80%\">").append(System.lineSeparator());
-
-            boolean isQuoted = false;
-            int quote = 0;
-            for (String e : list) {
-                if (!isQuoted) {
-                    table.append("<tr><td>");
-                }
-
-                for (int i = 0; i < e.length(); ++i) {
-                    char c = e.charAt(i);
-                    if (c == '"') {
-                        if (!isQuoted) {
-                            isQuoted = true;
-                            continue;
-                        } else {
-                            ++quote;
-                            if (quote % 2 == 0) {
-                                table.append(c);
-                            }
-                            if (i == e.length() - 1) {
-                                table.append("</td></tr><br/>").append(System.lineSeparator());
-                            }
-                            isQuoted = false;
-                            continue;
-                        }
-                    }
-
+            for (int i = 0; i < e.length(); ++i) {
+                char c = e.charAt(i);
+                if (c == '"') {
                     if (!isQuoted) {
-                        if (c == ',') {
-                            if (i == e.length() - 1) {
-                                table.append("</td></tr><br/>").append(System.lineSeparator());
-                                continue;
-                            }
-                            table.append("</td><td>");
-                            continue;
+                        isQuoted = true;
+                        continue;
+                    } else {
+                        ++quote;
+                        if (quote % 2 == 0) {
+                            table.append(c);
                         }
-                    }
-                    table.append(c);
-
-                    if (!isQuoted && i == e.length() - 1) {
-                        table.append("</td></tr><br/>").append(System.lineSeparator());
+                        if (i == e.length() - 1) {
+                            table.append("</td></tr><br/>").append(System.lineSeparator());
+                        }
+                        isQuoted = false;
+                        continue;
                     }
                 }
-            }
-            table.append("  </table>").append(System.lineSeparator());
-            table.append(" </body>").append(System.lineSeparator());
-            table.append("</html>").append(System.lineSeparator());
 
-            writer.print(table);
+                if (!isQuoted) {
+                    if (c == ',') {
+                        if (i == e.length() - 1) {
+                            table.append("</td></tr><br/>").append(System.lineSeparator());
+                            continue;
+                        }
+                        table.append("</td><td>");
+                        continue;
+                    }
+                }
+                table.append(c);
+
+                if (!isQuoted && i == e.length() - 1) {
+                    table.append("</td></tr><br/>").append(System.lineSeparator());
+                }
+            }
         }
+        table.append("  </table>").append(System.lineSeparator());
+        table.append(" </body>").append(System.lineSeparator());
+        table.append("</html>").append(System.lineSeparator());
+
+        FileReader.writeFile(table, args[1]);
     }
 }
+
