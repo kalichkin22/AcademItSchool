b3c27bc53b30578a5f6c2240784ac752088265d8
kalichkin22 <o.kalichkin@gmail.com>
2017-03-19 18:45:51 +0600

    Разбор CSV

--- Csv/src/ru/academits/kalichkin/csv/Main.java
+++ Csv/src/ru/academits/kalichkin/csv/Main.java
@@ -1,6 +1,7 @@
 package ru.academits.kalichkin.csv;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Main {
     private static final String HTML = "<html>";
@@ -21,89 +22,97 @@ public class Main {
 
 
     public static void main(String[] args) {
-        if (args.length != 2) {
-            throw new IllegalArgumentException ("Необходимо ввести два аргумента! " +
-                    "Первый аргумент имя исходного файла, второй аргумент - имя нового файла");
-        }
-
-        List<String> list = FileReader.readFile(args[0]);
-        StringBuilder table = new StringBuilder();
-
-        table.append(HTML).append(HEAD).append(META).append(TITLE).append(HEAD_CLOSE).append(BODY).append(TABLE).append(System.lineSeparator());
-
-        boolean isQuoted = false;
-        int quoteCount = 0;
-        int quoteCountAll = 0;
-
-        for (String e : list) {
-            if (!isQuoted) {
-                table.append(ROW);
+        try {
+            if (args.length != 2) {
+                throw new IllegalArgumentException();
             }
 
-            for (int i = 0; i < e.length(); ++i) {
-                char c = e.charAt(i);
-                if (c == QUOTE) {
-                    ++quoteCountAll;
-                    if (!isQuoted) {
-                        isQuoted = true;
-                        continue;
-                    } else {
-                        ++quoteCount;
-                        if (quoteCount % 2 != 0) {
-                            table.append(c);
-                        }
-                        if (i == e.length() - 1) {
-                            table.append(BREAK).append(System.lineSeparator());
-                        }
-                        isQuoted = false;
-                        continue;
-                    }
-                }
+            List<String> list = FileReader.readFile(args[0]);
+            StringBuilder table = new StringBuilder();
+
+            table.append(HTML).append(HEAD).append(META).append(TITLE).append(HEAD_CLOSE).append(BODY).append(TABLE).append(System.lineSeparator());
+
+            boolean isQuoted = false;
+            int quoteCount = 0;
+            int quoteCountAll = 0;
+
+            for (String e : list) {
                 if (!isQuoted) {
-                    if (c == SEPARATOR) {
-                        if (i == e.length() - 1) {
-                            table.append(DETAIL);
-                            table.append(BREAK).append(System.lineSeparator());
+                    table.append(ROW);
+                }
+
+                for (int i = 0; i < e.length(); ++i) {
+                    char c = e.charAt(i);
+                    if (c == QUOTE) {
+                        ++quoteCountAll;
+                        if (!isQuoted) {
+                            isQuoted = true;
+                            continue;
+                        } else {
+                            ++quoteCount;
+                            if (quoteCount % 2 != 0) {
+                                table.append(c);
+                            }
+                            if (i == e.length() - 1) {
+                                table.append(BREAK).append(System.lineSeparator());
+                            }
+                            isQuoted = false;
                             continue;
                         }
-                        table.append(DETAIL);
+                    }
+                    if (!isQuoted) {
+                        if (c == SEPARATOR) {
+                            if (i == e.length() - 1) {
+                                table.append(DETAIL);
+                                table.append(BREAK).append(System.lineSeparator());
+                                continue;
+                            }
+                            table.append(DETAIL);
+                            continue;
+                        }
+                    }
+
+                    if (c == '<') {
+                        table.append("&lt;");
                         continue;
                     }
-                }
+                    if (c == '>') {
+                        table.append("&gt;");
+                        continue;
+                    }
+                    if (c == '&') {
+                        table.append("&amp;");
+                        continue;
+                    }
 
-                if (c == '<') {
-                    table.append("&lt;");
-                    continue;
-                }
-                if (c == '>') {
-                    table.append("&gt;");
-                    continue;
-                }
-                if (c == '&') {
-                    table.append("&amp;");
-                    continue;
-                }
+                    table.append(c);
 
-                table.append(c);
+                    if (!isQuoted && i == e.length() - 1) {
+                        table.append(BREAK).append(System.lineSeparator());
+                    }
+                    if (isQuoted && i == e.length() - 1) {
+                        table.append(" ");
+                    }
 
-                if (!isQuoted && i == e.length() - 1) {
-                    table.append(BREAK).append(System.lineSeparator());
                 }
-                if (isQuoted && i == e.length() - 1) {
-                    table.append(" ");
-                }
-
             }
-        }
-        table.append(TABLE_CLOSE).append(BODY_CLOSE).append(HTML_CLOSE);
-        FileReader.writeFile(table, args[1]);
+            table.append(TABLE_CLOSE).append(BODY_CLOSE).append(HTML_CLOSE);
+            FileReader.writeFile(table, args[1]);
 
-        if (quoteCountAll % 2 != 0) {
-            System.out.println("Возможно Вы ввели лишнюю кавычку");
-            System.out.println("* Если ячейка таблицы содержит запятую или перевод строки, " + System.lineSeparator() +
+            if (quoteCountAll % 2 != 0) {
+                throw new NoSuchElementException();
+            }
+
+        } catch (NoSuchElementException e) {
+            System.out.println("Возможно Вы ввели лишнюю кавычку" + System.lineSeparator() +
+                    "* Если ячейка таблицы содержит запятую или перевод строки, " + System.lineSeparator() +
                     "* то в таком случае содержимое ячейки заключается в двойные кавычки." + System.lineSeparator() +
                     "* Если же ячейка содержит двойную кавычку, то эта кавычка дублируется.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Необходимо ввести два аргумента! " +
+                    "Первый аргумент имя исходного файла, второй аргумент - имя нового файла");
         }
     }
 }
+
 
