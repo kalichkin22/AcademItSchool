78be014ae8f4de3f68ba8b633df730194336040e
kalichkin22 <o.kalichkin@gmail.com>
2017-03-22 15:27:22 +0600

    Реализация хэш-таблицы

--- HashTable/src/ru/academits/kalichkin/main/Main.java
+++ HashTable/src/ru/academits/kalichkin/main/Main.java
@@ -12,10 +12,10 @@ public class Main {
     public static void main(String[] args) {
         HashTable<Integer> table = new HashTable<>(1, 5, 65, 3, 6, 35, null, 10);
 
-        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 12, 15));
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 65, null));
 
         //System.out.println(table.contains(7));
-        //System.out.println(table.removeAll(list));
+        System.out.println(table.removeAll(list));
         //System.out.println(table.remove(5));
         //System.out.println(table.retainAll(list));
         //System.out.println(table.containsAll(list));
