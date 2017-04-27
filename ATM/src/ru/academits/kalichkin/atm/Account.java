fb494ffe205183d7d7daa2e1d8d0274432db152a
kalichkin22 <o.kalichkin@gmail.com>
2017-04-27 23:49:32 +0600

    Реализация банкомата

--- ATM/src/ru/academits/kalichkin/atm/Account.java
+++ ATM/src/ru/academits/kalichkin/atm/Account.java
@@ -47,89 +47,30 @@ public class Account {
     }
 
     public void withDraw(int sum, int nominal) {
-        int countBanknote = sum / nominal;
-        int remainSum = sum % nominal;
-        boolean isPrintBalance = false;
-
-        if (sum % 50 != 0) {
-            System.out.println("Сумма должна быть кратна 50!");
-            return;
-        }
-
-        int newNominal = 0;
-        if (sum > 0) {
-            for (Banknotes banknotes : cash) {
-                if (nominal == banknotes.getNominal()) {
-                    if (remainSum == 0) {
-                        if (countBanknote <= banknotes.getCount()) {
-                            banknotes.setCount(banknotes.getCount() - countBanknote);
-                        } else if (countBanknote > banknotes.getCount()) {
-                            newNominal = sum - nominal * banknotes.getCount();
-                            banknotes.setCount(0);
-                        }
-                        isPrintBalance = true;
-                    } else {
-                        if (countBanknote <= banknotes.getCount()) {
-                            banknotes.setCount(banknotes.getCount() - countBanknote);
-                            newNominal = remainSum;
-                            sum = sum - (banknotes.getNominal() * countBanknote);
-                        } else if (countBanknote > banknotes.getCount()) {
-                            newNominal = sum - (banknotes.getNominal() * banknotes.getCount() + remainSum);
-                            banknotes.setCount(0);
-                            sum = sum - (banknotes.getNominal() * banknotes.getCount());
-                        }
-                        isPrintBalance = true;
-                    }
-                }
-            }
-
-            for (Banknotes banknotes : cash) {
-                if (newNominal == banknotes.getNominal()) {
-                    if (remainSum != 0) {
-                        int newCountBanknote = newNominal / remainSum;
-                        banknotes.setCount(banknotes.getCount() - newCountBanknote);
-                        sum = sum - banknotes.getNominal();
-                    } else {
-                        banknotes.setCount(banknotes.getCount() - 1);
-                        sum = sum - banknotes.getNominal();
-                    }
-                }
-            }
-        }
-        if (isPrintBalance) {
-            System.out.println("Баланс: " + getBalance());
-        } else {
-            System.out.println("Таких банкнот не существует!");
-        }
-    }
-
-
-    public void withDraw2(int sum, int nominal) {
-        int q = nominal;
-        int x = 0;
+        int newNominal = nominal;
+        int countBanknote;
 
         while (sum > 0) {
             for (int j = 0; j < cash.length; j++) {
-                x = q;
-                if (x == cash[j].getNominal()) {
-                    x = 0;
-                    x += sum / q;
-                    if (x <= cash[j].getCount()) {
-                        cash[j].setCount(cash[j].getCount() - x);
-                        sum = sum - cash[j].getNominal() * x;
+                if (newNominal == cash[j].getNominal()) {
+                    countBanknote = 0;
+                    countBanknote += sum / newNominal;
+                    if (countBanknote <= cash[j].getCount()) {
+                        cash[j].setCount(cash[j].getCount() - countBanknote);
+                        sum = sum - cash[j].getNominal() * countBanknote;
                         if (cash[j].getNominal() == cash[0].getNominal()) {
-                            q = cash[0].getNominal();
+                            newNominal = cash[0].getNominal();
                         } else {
-                            q = cash[j - 1].getNominal();
+                            newNominal = cash[j - 1].getNominal();
                         }
                     } else {
                         int count = cash[j].getCount();
                         sum = sum - cash[j].getNominal() * count;
                         cash[j].setCount(0);
                         if (cash[j].getNominal() == cash[0].getNominal()) {
-                            q = cash[0].getNominal();
+                            newNominal = cash[0].getNominal();
                         } else {
-                            q = cash[j - 1].getNominal();
+                            newNominal = cash[j - 1].getNominal();
                         }
 
                     }
