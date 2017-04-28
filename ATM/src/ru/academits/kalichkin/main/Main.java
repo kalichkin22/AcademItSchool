19bb3397f35c0ecbdc5fa1d4ffbdad359fa8cea5
kalichkin22 <o.kalichkin@gmail.com>
2017-04-28 15:12:02 +0600

    Реализация банкомата

--- ATM/src/ru/academits/kalichkin/main/Main.java
+++ ATM/src/ru/academits/kalichkin/main/Main.java
@@ -19,47 +19,42 @@ public class Main {
                     "4. Выйти"
             );
 
-            while (true) {
-                System.out.println("Выбирете номер операции: ");
-                int number = scanner.nextInt();
+            System.out.println("Выбирете номер операции: ");
+            int number = scanner.nextInt();
 
-                switch (number) {
-                    case 1:
-                        System.out.println("Баланс составляет: " + account.getBalance());
-                        break;
-                    case 2:
-                        Scanner scanner2 = new Scanner(System.in);
-
-                        System.out.println("Введите номинал банкноты: ");
-                        int nominalDeposit = scanner2.nextInt();
-
-                        System.out.println("Введите количество банкноты: ");
-                        int countDeposit = scanner2.nextInt();
-
-                        account.deposit(nominalDeposit, countDeposit);
-                        break;
-                    case 3:
-                        Scanner scanner3 = new Scanner(System.in);
-
-                        System.out.println("Введите сумму выдачи, кратную 50: ");
-                        int sum = scanner3.nextInt();
-
-                        System.out.println("Какими банкнотами произвести выдачу? ");
-                        int banknote = scanner3.nextInt();
-
-                        account.withDraw(sum, banknote);
-                        break;
-                    case 4:
-                        System.out.println("До свидания!");
-                        break;
-                    default:
-                        System.out.print("Неизвестная операция, попробуйте еще раз." + System.lineSeparator());
-                }
-
-                if (number == 4) {
+            switch (number) {
+                case 1:
+                    System.out.println("Баланс составляет: " + account.getBalance());
                     break;
-                }
+                case 2:
+                    Scanner scanner2 = new Scanner(System.in);
+
+                    System.out.println("Введите номинал банкноты: ");
+                    int nominalDeposit = scanner2.nextInt();
+
+                    System.out.println("Введите количество банкноты: ");
+                    int countDeposit = scanner2.nextInt();
+
+                    account.deposit(nominalDeposit, countDeposit);
+                    break;
+                case 3:
+                    Scanner scanner3 = new Scanner(System.in);
+
+                    System.out.println("Введите сумму выдачи, кратную 50: ");
+                    int sum = scanner3.nextInt();
+
+                    System.out.println("Какими банкнотами произвести выдачу? ");
+                    int banknote = scanner3.nextInt();
+
+                    account.withDraw(sum, banknote);
+                    break;
+                case 4:
+                    System.out.println("До свидания!");
+                    break;
+                default:
+                    System.out.print("Неизвестная операция, попробуйте еще раз." + System.lineSeparator());
             }
+
         } catch (InputMismatchException e) {
             System.out.println("Неверная операция, можно вводить только цифры.");
         }
