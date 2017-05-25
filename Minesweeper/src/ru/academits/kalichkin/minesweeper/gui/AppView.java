6bbf2026e88f8db6044e45eddac00b65c5938047
kalichkin22 <o.kalichkin@gmail.com>
2017-05-25 09:49:41 +0600

    сапер

--- Minesweeper/src/ru/academits/kalichkin/minesweeper/gui/AppView.java
+++ Minesweeper/src/ru/academits/kalichkin/minesweeper/gui/AppView.java
@@ -8,8 +8,6 @@ import ru.academits.kalichkin.minesweeper.model.Action;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
