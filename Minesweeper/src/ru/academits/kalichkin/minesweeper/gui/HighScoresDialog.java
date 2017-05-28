edb17206322b5e5e18cf593ec2877a3a9ab2c912
kalichkin22 <o.kalichkin@gmail.com>
2017-05-28 08:07:05 +0600

    сапер

--- Minesweeper/src/ru/academits/kalichkin/minesweeper/gui/HighScoresDialog.java
+++ Minesweeper/src/ru/academits/kalichkin/minesweeper/gui/HighScoresDialog.java
@@ -3,6 +3,8 @@ package ru.academits.kalichkin.minesweeper.gui;
 import ru.academits.kalichkin.minesweeper.model.PersonWin;
 
 import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
 import java.awt.*;
 import java.util.List;
 
@@ -32,10 +34,50 @@ class HighScoresDialog extends JDialog {
         }
 
         JTable table = new JTable(data, columnNames);
+        NumberedModel numberedModel = new NumberedModel(table.getModel());
+        table.setModel(numberedModel);
         table.setFont(new Font("TimesNewRoman", Font.PLAIN, 15));
         table.setEnabled(false);
         JScrollPane scrollPane = new JScrollPane(table);
         this.getContentPane().add(scrollPane);
         this.setVisible(true);
+    }
+
+
+    private static class NumberedModel extends AbstractTableModel {
+
+        private TableModel tableModel;
+
+        NumberedModel(TableModel tableModel) {
+            this.tableModel = tableModel;
+        }
+
+        @Override
+        public int getRowCount() {
+            return tableModel.getRowCount();
+        }
+
+        @Override
+        public int getColumnCount() {
+            return tableModel.getColumnCount() + 1;
+        }
+
+        @Override
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            if (columnIndex == 0) {
+                return rowIndex + 1;
+            } else {
+                return tableModel.getValueAt(rowIndex, columnIndex - 1);
+            }
+        }
+
+        @Override
+        public String getColumnName(int column) {
+            if (column == 0) {
+                return "Номер";
+            } else {
+                return tableModel.getColumnName(column - 1);
+            }
+        }
     }
 }
